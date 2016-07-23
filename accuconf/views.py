557e80c1e409b62bb28610837d1a98717a10ad10
Balachandran Sivakumar <balachandran@balachandran.org>
2016-07-23 10:45:00 +0530

    Initialise committee as a dict
    "committee" was initialised as a string. It should have been a
    dict. Additionally, committee.get("members") now returns None if the
    key isn't available.

--- accuconf/views.py
+++ accuconf/views.py
@@ -9,8 +9,8 @@ from flask import render_template, flash, redirect, url_for
 def index():
     if app.config.get("MAINTENANCE"):
         return redirect(url_for("maintenance"))
-    when_where = ""
-    committee = ""
+    when_where = {}
+    committee = {}
     venuefile = app.config.get('VENUE')
     committeefile = app.config.get('COMMITTEE')
 
@@ -24,7 +24,7 @@ def index():
         "title": "ACCU Conference 2017",
         "data": "Welcome to ACCU Conf 2017",
         "when_where": when_where,
-        "committee": committee.get("members")
+        "committee": committee.get("members", None)
     }
     return render_template("index.html", frontpage=frontpage)
 
