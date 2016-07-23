38b661194f6a6bd2a2a584a3a030f467913fffac
Russel Winder <russel@winder.org.uk>
2016-07-23 12:17:44 +0100

    Add a test to ensure we can use the Werkzeug test client in pytest.

--- tests/test_something_fatuous_but_nontheless_useful.py
+++ tests/test_something_fatuous_but_nontheless_useful.py
@@ -1,4 +1,7 @@
-# This test is (or was it is has been deleted) here just to test out the pytest capability on Travis-CI
+'''
+This test is (or was it is has been deleted) here just to test out the pytest capability on Travis-CI.
+'''
+
 
 def test_that_pytest_works():
     pass
