0d2a144c7c7108689c4ecdec7c8e9ad306c53af7
Russel Winder <russel@winder.org.uk>
2016-10-13 12:49:24 +0100

    Correct the Flask part deployment scripts.

--- flask_application_part/deploy_testconference.sh
+++ flask_application_part/deploy_testconference.sh
@@ -2,4 +2,4 @@
 
 chmod -R go+rX accuconf*
 
-rsync -rav --delete  --exclude __pycache__/ --exclude accuconf/nikola/static/ accuconf accuconf_config.py accuconf.wsgi conference@dennis.accu.org:/srv/testconference.accu.org/public/htdocs/
+rsync -rav --delete  --exclude __pycache__/ --exclude accuconf/static accuconf accuconf_config.py accuconf.wsgi conference@dennis.accu.org:/srv/testconference.accu.org/public/htdocs/
